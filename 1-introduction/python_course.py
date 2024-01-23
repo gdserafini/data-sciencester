24ce6cf5ba280c1cf1e100e55ab43b5f050362f7
gdserafini <gdinizserafini@gmail.com>
2024-01-23 10:38:22 -0300

    python 'course' updated

--- 1-introduction/python_course.py
+++ 1-introduction/python_course.py
@@ -12,3 +12,35 @@ def func_with_doc(param: Any) -> Any:
     return param
 
 print(func_with_doc(1))
+
+#default param
+def first_class_func(func = lambda x: x):
+    return func(1)
+
+func = func_with_doc
+print(first_class_func(func))
+print(first_class_func(lambda x: x + 1)) #passa uma função def f(x): return x+1
+double = lambda x: x * 2 #não recomendado 
+
+x = 1
+def addone(num):
+    num += 1
+addone(x)
+print(x)
+
+string_ex = '\t'
+string_ex_r = r'\t'
+print(len(string_ex), " ", len(string_ex_r))
+print(
+    """
+    Multi 
+Line 
+        String
+    """
+)
+print(f'f-string: {1}, {2}, {3}')
+
+#try:
+#    print(0/0)
+#except Exception as ex:
+#    raise ex
